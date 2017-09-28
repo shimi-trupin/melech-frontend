30610a163d7d72e7171747163234654447035ae9
urielrabii <urielrab@gmail.com>
2017-09-28 17:02:43 +0300

    gant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from "./app.routing.module";
-import {ShcedulerComponent} from "./shceduler/shceduler.component";
-import {AddActivityComponent} from "./add-activity/add-activity.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {GantChartComponent} from "./gant-chart/gant-chart.component";
+import {AppRoutingModule} from './app.routing.module';
+import {ShcedulerComponent} from './shceduler/shceduler.component';
+import {AddActivityComponent} from './add-activity/add-activity.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {GantChartComponent} from './gant-chart/gant-chart.component';
+import {GanttModule} from './gantt.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {GantChartComponent} from "./gant-chart/gant-chart.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GanttModule
   ],
   providers: [],
   bootstrap: [AppComponent]
